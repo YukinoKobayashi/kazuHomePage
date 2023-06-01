1b6c483d8fd0e2f868b84780a822511bd9c39080
YukinoKobayashi <65746243+YukinoKobayashi@users.noreply.github.com>
2023-06-01 21:59:22 +0900

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -1,3 +1,10 @@
 $(function(){
 $("#header").load("header.html");
 });
+
+
+$(document).ready(function() {
+    if(location.pathname != "/") {
+        $('.nav li a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+    } else $('.nav li a:eq(0)').addClass('active');
+});
